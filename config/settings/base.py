2f577f57b2a5759df8b2b30ae2fde4c732dfe889
xcheche <chekwubechukwu23@gmail.com>
2025-07-26 06:02:48 +0100

    logger
    logger setup

--- config/settings/base.py
+++ config/settings/base.py
@@ -160,3 +160,27 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 #Any  url with this will have cors headers added to it
 CORS_URLS_REGEX = r"^api/.*$"  # Adjust this regex to match your allowed origins
+
+
+#Logging configuration
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "%(levelname)s %(name)-12s %(asctime)s %(module)s "
+                      "%(process)d %(thread)d %(message)s",
+        }
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        }
+    },
+    "root": {
+        "level": "INFO",
+        "handlers": ["console"]
+    }
+}
